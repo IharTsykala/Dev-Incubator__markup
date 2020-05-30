1c98cc3582213a4c24c92b5fc9e4ba89d51d7601
Ihar Tsykala <Sudja09@bk.ru>
2020-05-30 17:41:15 +0300

    feat: add base structure in data-section

--- src/Pages/MainPage/DataSection/DataSection.tsx
+++ src/Pages/MainPage/DataSection/DataSection.tsx
@@ -5,7 +5,30 @@ import React from "react"
 // }
 
 const DataSection: React.FunctionComponent = () => {
-  return <div className={"main-page"}>DataSection</div>
+  return (
+    <section className={"data-section"}>
+      <form className={"form-private-data"}>
+        <input className={"form-private-data__input"} />
+        <input className={"form-private-data__input"} />
+        <input className={"form-private-data__input"} />
+        <input className={"form-private-data__input"} />
+        <button className={"form-private-data__button"} />
+      </form>
+      <article className={"our-contacts"}>
+        <div className={"our-contacts__address-phone"}>
+          <p className={"our-contacts__text"}></p>
+          <p className={"our-contacts__text"}></p>
+          <p className={"our-contacts__text"}></p>
+          <p className={"our-contacts__text"}></p>
+        </div>
+        <div className={"our-contacts__internet"}>
+          <p className={"our-contacts__text"}></p>
+          <p className={"our-contacts__text"}></p>
+        </div>
+        <img className={"our-contacts__internet"} />
+      </article>
+    </section>
+  )
 }
 
 // const mapStateToProps = (state: any) => ({
