cab1031122551cbbf76d3411791eeb5e6b55910a
Ihar Tsykala <Sudja09@bk.ru>
2020-06-01 11:12:34 +0300

    feat: add photo reducer and connect to store

--- src/Redux/index.ts
+++ src/Redux/index.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux"
 import { mapCenterReducer } from "./store/map/map.reduser"
+import { photoReducer } from "./store/photo/photo.reduser"
 
 export default combineReducers({
   mapCenter: mapCenterReducer,
+  photo: photoReducer,
 })
