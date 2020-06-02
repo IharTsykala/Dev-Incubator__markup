fcf8fc9070090bda4ba6ed66dcee1ba0bc036742
Ihar Tsykala <Sudja09@bk.ru>
2020-06-02 13:15:39 +0300

    Updates

---
+++ precache-manifest.56cee476fd82ea4f5720880569a2007e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe4ca05a3e86224497a96fd5a30d7e7c",
+    "url": "/Dev-Incubator__markup/index.html"
+  },
+  {
+    "revision": "77470fd8b123310f3024",
+    "url": "/Dev-Incubator__markup/static/css/main.bf1092ab.chunk.css"
+  },
+  {
+    "revision": "bc410916b02d9ee24478",
+    "url": "/Dev-Incubator__markup/static/js/2.cb8e72ce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Dev-Incubator__markup/static/js/2.cb8e72ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77470fd8b123310f3024",
+    "url": "/Dev-Incubator__markup/static/js/main.df9cb92c.chunk.js"
+  },
+  {
+    "revision": "022b4122106313f24349",
+    "url": "/Dev-Incubator__markup/static/js/runtime-main.382cdcdb.js"
+  },
+  {
+    "revision": "0392a14b1da29f37f495c9bc9248d01c",
+    "url": "/Dev-Incubator__markup/static/media/backgroundWithWinter.0392a14b.png"
+  },
+  {
+    "revision": "a2b324fab575f2b63e0fc72c8d678f8f",
+    "url": "/Dev-Incubator__markup/static/media/book.a2b324fa.png"
+  },
+  {
+    "revision": "a6ed31ce1b7e784c5155526a281df2e3",
+    "url": "/Dev-Incubator__markup/static/media/bottles.a6ed31ce.png"
+  },
+  {
+    "revision": "d510c48ee731001cc0b5fe59758424bf",
+    "url": "/Dev-Incubator__markup/static/media/businessCards.d510c48e.png"
+  },
+  {
+    "revision": "e67cba7f4791cb155e68fe08b5f98bf6",
+    "url": "/Dev-Incubator__markup/static/media/cake.e67cba7f.png"
+  },
+  {
+    "revision": "eac0e644280de2d7a72e84154b6a3598",
+    "url": "/Dev-Incubator__markup/static/media/healingTea.eac0e644.png"
+  },
+  {
+    "revision": "5f1f3e1b4fcd3921316fa9225e1bb66d",
+    "url": "/Dev-Incubator__markup/static/media/manWithBigBeard.5f1f3e1b.png"
+  },
+  {
+    "revision": "cd2e58ab94a1ee92fb08d779e42680cc",
+    "url": "/Dev-Incubator__markup/static/media/manWithSmallBeard.cd2e58ab.png"
+  },
+  {
+    "revision": "77a8927d54b9177a9baf3df9bd9a66b3",
+    "url": "/Dev-Incubator__markup/static/media/manWithoutBeard.77a8927d.png"
+  },
+  {
+    "revision": "98377b428b7a6f754e7ceee728504ca1",
+    "url": "/Dev-Incubator__markup/static/media/mathematik.98377b42.png"
+  },
+  {
+    "revision": "4e943abb15bd372cb49e428f61de5ba0",
+    "url": "/Dev-Incubator__markup/static/media/officePlace.4e943abb.png"
+  },
+  {
+    "revision": "0c1a3bc3bc11f9d62746908eb251632f",
+    "url": "/Dev-Incubator__markup/static/media/womanWithGlasses.0c1a3bc3.png"
+  },
+  {
+    "revision": "9dc7812a9cd975d7bd08a70ed9e73108",
+    "url": "/Dev-Incubator__markup/static/media/womanWithoutGlasses.9dc7812a.png"
+  }
+]);
